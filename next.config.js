7363c9effbeaf4da1ad2f970f73ef90d9b0e457e
bysnn <bysnn@qq.com>
2022-07-29 14:03:38 +0800

    change build dir

--- next.config.js
+++ next.config.js
@@ -8,21 +8,22 @@ const nextConfig = {
   lessLoaderOptions: {
   },
   webpack: (config, { isServer }) => {
-      config.resolve.alias['components'] = path.join(__dirname, 'components');
-      config.resolve.alias['content'] = path.join(__dirname, 'content');
-      config.resolve.alias['utils'] = path.join(__dirname, 'utils');
-      config.resolve.alias['api'] = path.join(__dirname, 'api');
-      config.resolve.alias['@root'] = path.join(__dirname, '/');
-      return config;
+    config.resolve.alias['components'] = path.join(__dirname, 'components');
+    config.resolve.alias['content'] = path.join(__dirname, 'content');
+    config.resolve.alias['utils'] = path.join(__dirname, 'utils');
+    config.resolve.alias['api'] = path.join(__dirname, 'api');
+    config.resolve.alias['@root'] = path.join(__dirname, '/');
+    return config;
   },
   env: {
-      ENVHOST: process.env.ENV ? envHostMapping[process.env.ENV] : 'dev-',
-      CLIENT_ID: process.env.ENV ? tokenClientIdMapping[process.env.ENV] : 'MPS.Portal.Debug',
-      CLIENT_SECRET: process.env.ENV ? tokenClientSecretMapping[process.env.ENV] : 'IEYVtrSGekyI',
-      CONFIGURATION_API_URL: process.env.ENV ? configurationApiUrlMapping[process.env.ENV] : 'https://dev-mps-configuration-api.ysd.com/api/v1',
-      TOKEN_API_URL: process.env.ENV ? tokenApiUrlMapping[process.env.ENV] : 'https://stg-auth.ysd.com:44300/connect/token'
+    ENVHOST: process.env.ENV ? envHostMapping[process.env.ENV] : 'dev-',
+    CLIENT_ID: process.env.ENV ? tokenClientIdMapping[process.env.ENV] : 'MPS.Portal.Debug',
+    CLIENT_SECRET: process.env.ENV ? tokenClientSecretMapping[process.env.ENV] : 'IEYVtrSGekyI',
+    CONFIGURATION_API_URL: process.env.ENV ? configurationApiUrlMapping[process.env.ENV] : 'https://dev-mps-configuration-api.ysd.com/api/v1',
+    TOKEN_API_URL: process.env.ENV ? tokenApiUrlMapping[process.env.ENV] : 'https://stg-auth.ysd.com:44300/connect/token'
 
-  }
+  },
+  distDir: 'build',
 };
 
 module.exports = withLess(nextConfig);
